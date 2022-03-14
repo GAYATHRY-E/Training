263183577f7b3e796e24b6077b21411d409f50b4
Gayathry.E <57176258+GAYATHRY-E@users.noreply.github.com>
2022-03-14 14:19:33 +0530

    Update StudentRepo.java

--- src/main/java/com/example/demo/StudentRepo.java
+++ src/main/java/com/example/demo/StudentRepo.java
@@ -1,7 +1,16 @@
+/**
+ * Spring Boot SubMission form for students.
+ * class name: StudentReo
+ * description : Repository class for SubMission form.
+ * @author GAYATHRY E
+ * @version jdk 8
+ *
+ */
+
+
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepo extends JpaRepository<Students, Integer>
 {
